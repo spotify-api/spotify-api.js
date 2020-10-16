41a2465140d7d9043f8032e518333a0a4d08bc04
Sfue <50198413+abh80@users.noreply.github.com>
2020-10-16 08:54:23 +0530

    minor changes

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,1 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Client = exports.version = void 0;
-const Client_1 = __importDefault(require("./Client"));
-exports.Client = Client_1.default;
-const version = '3.0.2';
-exports.version = version;
-//# sourceMappingURL=index.js.map
+"use strict";var __importDefault=this&&this.__importDefault||function(e){return e&&e.__esModule?e:{default:e}};Object.defineProperty(exports,"__esModule",{value:!0}),exports.Client=exports.version=void 0;const Client_1=__importDefault(require("./Client"));exports.Client=Client_1.default;const version="3.0.2";exports.version="3.0.2";
