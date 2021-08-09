57a21d05ce4c9c9e8d1de5a14a0b95a9c2d5afbb
Scientific-Guy <69678617+Scientific-Guy@users.noreply.github.com>
2021-08-09 16:00:52 +0530

    feat: added handle for 429 requests.

--- src/Client.ts
+++ src/Client.ts
@@ -1,4 +1,5 @@
-import { ClientOptions } from "./Interface";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { ClientOptions, FetchOptions } from "./Interface";
 
 /**
  * The basic client to interact with the Spotify Web API.
@@ -11,15 +12,50 @@ export class Client {
     public token!: string;
 
     /**
+     * The version of spotify web api. For future purposes.
+     */
+    public version: `v${number}` = 'v1';
+
+    /**
      * The basic client to interact with the Spotify Web API.
      * 
      * @param options The options necessary for the client.
-     * @example
-     * const client = new Client({ token: "someToken" });
+     * @example const client = new Client({ token: "someToken" });
      */
     public constructor(options: ClientOptions | string) {
         if (typeof options == "string") this.token = options;
         else if (typeof options.token == "string") this.token = options.token;
+    }
+
+    /**
+     * Used to fetch data from spotify rest api.
+     * 
+     * @param url The path from spotify api to fetch!
+     * @param options The additional options required to fetch from the api.
+     * @example await client.fetch('/users/id');
+     */
+    public async fetch(url: string, options: FetchOptions = {}) {
+        try {
+            const response = await axios({
+                url: `https://api.spotify.com/${this.version}${url}`,
+                method: options.method || 'GET',
+                params: options.params,
+                headers: {
+                    Authorization: "Bearer " + this.token,
+                    Accept: 'application/json',
+                    ...options.headers
+                },
+                data: options.body
+            });
+    
+            return response.data;
+        } catch(error) {
+            if (error.status == 429) {
+                const retryAfter = error.headers['Retry-After'];
+                if (typeof retryAfter == "number") await new Promise(r => setTimeout(r, retryAfter * 1000));
+                return this.fetch(url, options)
+            } else throw error;
+        }
     }
 
 }
--- src/Interface.ts
+++ src/Interface.ts
@@ -1,6 +1,30 @@
+/**
+ * All the spotify web api methods.
+ */
+export type Methods = 'GET' | 'POST' | 'DELETE' | 'PUT' | 'PATCH';
+
+/**
+ * The options required for the Client.
+ */
 export interface ClientOptions {
+    /** The ready event which is called when the token is aquired. */
     onReady?: () => void;
+    /** The refresh which is called when there is a token refresh. */
     onRefresh?: () => void;
+    /** Set true to refresh token if the token is needed to be acquired else put the refresh token here. */
     refreshToken?: boolean | string;
+    /** Your spotify web api token. */
     token?: string;
+}
+
+/** The options necessary for the fetch function in Client. */
+export interface FetchOptions {
+    /** The headers to apply. */
+    headers?: Record<string, string>;
+    /** The method type. */
+    method?: Methods;
+    /** Search query parameters. */
+    params?: Record<string, string>;
+    /** The json body to send if available. */
+    body?: Record<string, string | boolean | number>;
 }
